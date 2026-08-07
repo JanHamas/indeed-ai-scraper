60f80382408622871ec1c41a600d635f77623b21
JanHamas <hamasjan82@gmail.com>
2026-08-07 23:27:15 +0500

    asdf

--- my_actor/config.py
+++ my_actor/config.py
@@ -39,7 +39,7 @@ class ScraperSettings:
 
     EMAIL="hamasjan82@gmail.com"
     EMAIL_PASSWORD="fbqzdwrtfkaqjjij"
-    TO_EMAIL="hamasjan74@gmail.com"
+    TO_EMAIL="hamasjan833@gmail.com"
 
 
     # ── Debugging ───────────────────────────────────────────────────────
